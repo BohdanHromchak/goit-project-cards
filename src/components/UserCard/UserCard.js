60d72246c13c543bef3c64ebe7a0ccb54a9be2e4
BohdanHromchak <gromchakbogdan@gmail.com>
2023-04-23 21:16:53 +0300

    add images

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import css from './UserCard.module.css'
+import { ReactComponent as GoitLogo } from "../../images/goitLogo.svg";
+// import { ReactComponent as CardImage } from "../../images/cardImage.png";
+import cardImage from '../../images/cardImage.png'
 
 export const UserCard = ({ tweets, followers, avatar, user }) => {
-    console.log(avatar)
-  return <li className={css.cardItem}><img src={avatar} alt={user} className={css.userImg}/></li>;
+
+  return <li className={css.cardItem}>
+       <GoitLogo />
+       <img src={cardImage} alt='cardImage'/>
+    <img src={avatar} alt={user} className={css.userImg}/></li>;
 };
